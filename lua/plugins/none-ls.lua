bd8f444cd035b719d3e6af3a7c4dc49ccb392706
Remus <remus.rughinis.007@gmail.com>
2024-07-10 16:07:28 +0300

    solved bug with eslint_d from none-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,5 +1,8 @@
 return {
 	"nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim"
+  },
 
 	config = function()
 		local null_ls = require("null-ls")
@@ -10,7 +13,7 @@ return {
 
         -- javascript
 				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.diagnostics.eslint_d,
+        require("none-ls.diagnostics.eslint_d"),
 			},
 		})
 
